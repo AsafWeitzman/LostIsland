5a4a4b22c3d80812081734566fd3548009898e20
AsafWeitzman <62206411+AsafWeitzman@users.noreply.github.com>
2021-07-17 00:36:27 +0300

    shadow controller camera fixed

--- Assets/Scripts/sunScript.cs
+++ Assets/Scripts/sunScript.cs
@@ -9,7 +9,7 @@ public class sunScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(Vector3.zero, Vector3.right, 2 * Time.deltaTime);
+        transform.RotateAround(Vector3.zero, Vector3.right, (float)0.8 * Time.deltaTime);
         transform.LookAt(Vector3.zero);
     }
 }
